a7dfa9f84b5e7758b27108f202c87af9f55d514a
andrealfian420 <andrealfian69699@gmail.com>
2020-12-04 13:24:56 +0700

    Changing default page title

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,12 @@
+import { useEffect } from 'react';
 import LoginLeftSection from '../components/LoginLeftSection';
 import ImageRightSection from '../components/ImageRightSection';
 
 const Login = () => {
+  useEffect(() => {
+    document.title = 'Login';
+  });
+
   return (
     <>
       <LoginLeftSection />
