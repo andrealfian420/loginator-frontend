9a68b5c3bb104b28a30e817c5193539d0fbfbdb8
andrealfian420 <andrealfian69699@gmail.com>
2020-12-07 22:44:10 +0700

    Adding form validation for register form

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,7 +1,15 @@
+import { useForm } from 'react-hook-form';
+
 const RegisterForm = () => {
+  const { register, errors, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
     <div className="flex justify-center items-center xl:block">
-      <form className="md:mx-4 w-full max-w-md mt-12 mb-4">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="md:mx-4 w-full max-w-md mt-12 mb-4"
+      >
         <div className="grid grid-cols-6 gap-x-2 mb-4">
           <div className="col-span-3">
             <label
@@ -15,7 +23,23 @@ const RegisterForm = () => {
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               placeholder="Masukkan nama depan"
               id="firstName"
+              name="firstName"
+              ref={register({
+                required: {
+                  value: true,
+                  message: 'Please fill this field',
+                },
+                maxLength: {
+                  value: 50,
+                  message: 'The first name is too long',
+                },
+              })}
             />
+            {errors.firstName && (
+              <span className="text-sm font-semibold text-red-500">
+                {errors.firstName.message}
+              </span>
+            )}
           </div>
           <div className="col-span-3">
             <label
@@ -29,7 +53,23 @@ const RegisterForm = () => {
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               placeholder="Masukkan nama belakang"
               id="lastName"
+              name="lastName"
+              ref={register({
+                required: {
+                  value: true,
+                  message: 'Please fill this field',
+                },
+                maxLength: {
+                  value: 50,
+                  message: 'The last name is too long',
+                },
+              })}
             />
+            {errors.lastName && (
+              <span className="text-sm font-semibold text-red-500">
+                {errors.lastName.message}
+              </span>
+            )}
           </div>
         </div>
         <div className="mb-4">
@@ -44,7 +84,23 @@ const RegisterForm = () => {
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             placeholder="Masukkan username anda"
             id="username"
+            name="userName"
+            ref={register({
+              required: {
+                value: true,
+                message: 'Please fill this field',
+              },
+              maxLength: {
+                value: 20,
+                message: 'The username is too long',
+              },
+            })}
           />
+          {errors.userName && (
+            <span className="text-sm font-semibold text-red-500">
+              {errors.userName.message}
+            </span>
+          )}
         </div>
         <div className="mb-4">
           <label
@@ -54,11 +110,24 @@ const RegisterForm = () => {
             Email
           </label>
           <input
-            type="email"
+            type="text"
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             placeholder="Masukkan email anda"
             id="email"
+            name="email"
+            ref={register({
+              required: { value: true, message: 'Please fill this field' },
+              pattern: {
+                value: /^[^@]+@[^@]+\.[^@]+$/,
+                message: 'Invalid email format. example: john@doe.com',
+              },
+            })}
           />
+          {errors.email && (
+            <span className="text-sm font-semibold text-red-500">
+              {errors.email.message}
+            </span>
+          )}
         </div>
         <div className="mb-2">
           <label
@@ -72,7 +141,25 @@ const RegisterForm = () => {
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             placeholder="Masukkan password anda"
             id="password"
+            name="password"
+            ref={register({
+              required: { value: true, message: 'Please fill this field' },
+              minLength: {
+                value: 8,
+                message: 'Password should contain at least 8 characters',
+              },
+              pattern: {
+                value: /^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*])[\w!@#$%^&*]{8,}$/,
+                message:
+                  'Password should at least have one lower case, one upper case, one of the following unique character (!@#$%^&*) and a number',
+              },
+            })}
           />
+          {errors.password && (
+            <span className="text-sm font-semibold text-red-500">
+              {errors.password.message}
+            </span>
+          )}
         </div>
 
         <button
